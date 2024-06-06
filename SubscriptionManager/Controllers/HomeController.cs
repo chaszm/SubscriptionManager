c460fa0f0529b6c5504506b5169ca117d92061ca
Chase <123278965+chaszm@users.noreply.github.com>
2024-06-06 13:42:25 -0400

    added sql database connection and tabs

--- SubscriptionManager/Controllers/HomeController.cs
+++ SubscriptionManager/Controllers/HomeController.cs
@@ -13,14 +13,14 @@ namespace SubscriptionManager.Controllers
             return View();
         }
 
-        public ActionResult About()
+        public ActionResult Analytics()
         {
             ViewBag.Message = "Your application description page.";
 
             return View();
         }
 
-        public ActionResult Contact()
+        public ActionResult History()
         {
             ViewBag.Message = "Your contact page.";
 
---
+++ SubscriptionManager/Models/Subscription.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SubscriptionManager.Models
+{
+    public class Subscription
+    {
+        public int Id { get; set; }
+        public string name { get; set; }
+
+        public string logo { get; set; }
+
+        public int amount { get; set; }
+
+        public DateTime length { get; set; }
+
+        public DateTime startDate { get; set; }
+
+        public string status { get; set; }
+
+        public string siteLink { get; set; }
+
+
+        public Subscription()
+        {
+
+
+        }
+
+
+
+    }
+}
